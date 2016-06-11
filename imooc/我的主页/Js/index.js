f05cbd24caeb37f2b01cc9100b0d3b7621b88bd0
aleilei123 <1395549036@qq.com>
2016-06-11 10:51:44 +0800

    imooc

---
+++ imooc/我的主页/Js/index.js
@@ -0,0 +1,10 @@
+(function(){
+$drag=$('#drag');
+var dragweidth=$('drag').style.left;
+function centerdrag(id){
+var windouwidth=document.documentElement.clientWidth;
+var windowheight=document.documentElement.clientHeight;
+
+ // var dragweidth=$(id).style.
+}
+})
